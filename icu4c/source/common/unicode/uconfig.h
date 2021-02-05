77d4f153a6b34aa63f51437e2d89e76f1ba34872
Rob Houston <rhouston@esri.com>
2021-02-05 12:02:55 +0000

    Initial test

--- icu4c/source/common/unicode/uconfig.h
+++ icu4c/source/common/unicode/uconfig.h
@@ -45,10 +45,14 @@
  * @stable ICU 2.4
  */
 
-// RTC defines
+ //RTC defines
+#ifndef U_COMMON_IMPLEMENTATION
 #define U_COMMON_IMPLEMENTATION // build the icu common library
+#endif
+#ifndef U_STATIC_IMPLEMENTATION
 #define U_STATIC_IMPLEMENTATION // build icu as a static library
-// end RTC defines
+#endif // !U_STATIC_IMPLEMENTATION
+ //end RTC defines
 
 /**
  * If this switch is defined, ICU will attempt to load a header file named "uconfig_local.h"
